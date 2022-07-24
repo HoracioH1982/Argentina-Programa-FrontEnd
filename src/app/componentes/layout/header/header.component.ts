ed33589055eaf56efe511ebfed6c9cfe7eaf85fe
Horacio <horacionhernandez@gmail.com>
2022-07-24 02:47:32 -0300

    Agrego style local, binding y html header

--- src/app/componentes/layout/header/header.component.ts
+++ src/app/componentes/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  headerList:any;
+  redesList:any;
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
-  }
+    this.datosPortfolio.obtenerDatos().subscribe(data=>{
+      console.log(data);
+      this.headerList=data.habilidades;    
+      this.redesList=data.redes_sociales;
+  })
+}
 
 }
